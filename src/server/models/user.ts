8e0944c35ce2bdc6d9e9977a6a0dbbb20160a3a7
Orland Contreras <orland102@gmail.com>
2019-06-01 18:52:02 -0500

    RestServer API User

---
+++ src/server/models/user.ts
@@ -0,0 +1,74 @@
+import { Document, Schema, Model, model } from 'mongoose';
+
+import uniqueValidator from 'mongoose-unique-validator';
+
+// Roles valid list
+let validRoles = {
+    values: [
+        'ADMIN_ROLE',
+        'USER_ROLE'
+    ],
+    message: '{VALUE} is not valid role'
+}
+
+// Interface of the model
+export interface IUserModel extends Document {
+    name: string;
+    email: string;
+    password: string;
+    img?: string;
+    role?: string;
+    state?: string;
+    google?: string;
+}
+
+// Schema definition
+export let UserSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Field name is required']
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'Field email is required']
+    },
+    password: {
+        type: String,
+        required: [true, 'Fields user and password are required']
+    },
+    img: {
+        type: String,
+        required: false 
+    },
+    role: {
+        type: String,
+        default: 'USER_ROLE',
+        enum: validRoles
+    },
+    state: {
+        type: Boolean,
+        default: true
+
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }    
+});
+
+// delete field password from json response
+UserSchema.methods.toJSON = function() {
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
+
+    return userObject;
+}
+
+// Add a validator to the unique fields
+UserSchema.plugin(uniqueValidator, {
+    message: '{PATH} must be unique'
+});
+
+export const User: Model<IUserModel> = model<IUserModel>("User", UserSchema); 
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,13 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import appUserRoute from './routes/user';
 
 // Working with Express()
 export const server = () => {
     const app = express();
+    let urlDB: string;
 
-    if(process.env.NODE_ENV !== 'production') {
-        dotenv.config();
+    dotenv.config();
+
+    if (process.env.NODE_ENV === 'dev') {
+        urlDB = <string>process.env.DATABASE_DEV;
+    }else {
+        urlDB = <string>process.env.DATABASE_PROD;
     }
 
     // parse application/x-www-form-urlencoded
@@ -15,45 +22,28 @@ export const server = () => {
 
     // parse application/json
     app.use(bodyParser.json());
-    
-     app.get('/user', ( req, res ) => {
-        
-        res.json('get user');
-    });
-    
-    app.post('/user', ( req, res ) => {
 
-        let body = req.body;
-
-        if (body.name === undefined) {
-            res.status(400).json({
-                ok: false,
-                message: 'The name is required'
-            });
-        } else {
-            
-            res.json({
-                body
-            });
-        }
-        
-    });
-
-    app.put('/user/:id', ( req, res ) => {
-
-        let id = req.params.id;
-        
-        res.json({
-            id
-        });
-    });
+    // Routes
+    app.use(appUserRoute); // User
     
-    app.delete('/user', ( req, res ) => {
-        
-        res.json('delete user');
+
+    
+    // connect to mongoDB
+    mongoose.connect(
+        urlDB, 
+        { 
+            useNewUrlParser: true,
+            useCreateIndex: true
+        },
+        (err) => {
+    
+            if (err) {
+                throw err;
+            }
+
+            console.log('Database online!');
+    
     });
-    
-    
 
     app.listen(process.env.PORT, () => {
         console.log(`Listening port ${ process.env.PORT }`);
